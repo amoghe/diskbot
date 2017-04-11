49bf85cede2a6b675d6100f3f1ef8c7669798e34
Akshay Moghe <akshay.moghe@gmail.com>
2017-04-10 17:21:33 -0700

    Ad ability build ISO using grub
    Now we can pick whether the ISO should boot using grub or
    isolinux as the bootloader.

--- src/iso_builder.rb
+++ src/iso_builder.rb
@@ -9,12 +9,29 @@ class IsoBuilder < BaseBuilder
     raise ArgumentError, 'Bad output file' unless output_path
     @rootfs_path = rootfs_path
     @output_path = output_path
+
+    @bootloader = "grub" # TODO: make this configurable
+    @bootmode   = "bios" # TODO: make this configurable
+   end
+
+  def build
+    case @bootloader
+    when "isolinux"
+      notice "Building ISO using isolinux as bootloader"
+      self.build_isolinux()
+    when "grub"
+      notice("Building ISO using grub as bootloader")
+      self.build_grub()
+    else
+      notice("Building ISO using grub as bootloader")
+      self.build_grub()
+    end
   end
 
   ##
+  # Build the iso with isolinux as the bootloader
   #
-  #
-  def build()
+  def build_isolinux()
     info("Ensure (temporary) workspace dirs exist")
     work_dirs = [ 'unpacked', 'iso', 'iso/isolinux', 'iso/live', 'tools' ]
     work_dirs.each { |dir| execute!("mkdir #{dir}", false) }
@@ -67,6 +84,52 @@ class IsoBuilder < BaseBuilder
   end
 
   ##
+  #
+  #
+  def build_grub
+    info("Ensure (temporary) workspace dirs exist")
+    work_dirs = [ 'unpacked', 'iso', 'iso/boot/grub', 'iso/live', 'tools' ]
+    work_dirs.each { |dir| execute!("mkdir -p #{dir}", false) }
+
+    info("Unpacking the rootfs to prepare it for live booting")
+    execute!("tar -xzf #{@rootfs_path} -C unpacked")
+
+    info("Installing live-boot pkgs")
+    execute!("chroot unpacked apt-get update")
+    execute!("chroot unpacked apt-get --yes install live-boot")
+    execute!("chroot unpacked apt-get --yes install live-boot-initramfs-tools")
+    execute!("chroot unpacked update-initramfs -u")
+
+    info("Squashing the modified rootfs")
+    execute!("mksquashfs unpacked iso/live/root.squashfs -no-progress")
+
+    info("Copying kernel and initrd into iso dir")
+    execute!("cp unpacked/vmlinuz iso/live/vmlinuz")
+    execute!("cp unpacked/initrd.img iso/live/initrd.img")
+
+    download_unpack_grub_tools('tools')
+
+    info("Writing out isolinux config file")
+    File.open("iso/boot/grub/grub.cfg", 'w') { |f|
+      f.write(self.grub_cfg_contents())
+    }
+
+    grub_arch = (@bootmode == "bios") ? "i386-pc" : "x86_64-efi"
+    info("Using grub arch: #{grub_arch}")
+
+    info("Creating ISO (using grub-mkrescue)")
+    execute!(["grub-mkrescue",
+      "-d tools/usr/lib/grub/#{grub_arch}",
+      "-o #{@output_path}",
+      "./iso",].join(" "))
+
+  ensure
+    info("deleting (temporary) work dirs")
+    work_dirs.each { |d| execute!("rm -rf #{d}") }
+    nil
+  end
+
+  ##
   # Download and unpack the isolinux tools into the specified dir.
   #
   def download_unpack_isolinux_tools(dir)
@@ -75,12 +138,24 @@ class IsoBuilder < BaseBuilder
     Dir.glob("#{dir}/*.deb") { |pkg| execute!("dpkg-deb --extract #{pkg} #{dir}") }
   end
 
+  def download_unpack_grub_tools(dir)
+    grubpkgname = (@bootmode == "bios") ? "grub-pc-bin" : "grub-efi-amd64-bin"
+		execute!("mkdir -p #{dir}", false)
+		execute!("cd #{dir} && apt-get download grub-common", false)
+		execute!("cd #{dir} && apt-get download #{grubpkgname}", false)
+		Dir.glob("#{dir}/*.deb") { |pkg| execute!("dpkg-deb --extract #{pkg} #{dir}") }
+	end
+
+  ##
+  # File contents for the isolinux config file
+  #
   def isolinux_cfg_contents
     return [
       "UI menu.c32",
       "PROMPT LiveCD",
       "DEFAULT 1",
       "TIMEOUT 15",
+      "MENU RESOLUTION 1024 768",
       "",
       "LABEL 1",
       "  MENU DEFAULT",
@@ -93,6 +168,24 @@ class IsoBuilder < BaseBuilder
       "  KERNEL /live/vmlinuz",
       "  APPEND initrd=/live/initrd.img boot=live",
       "",
+    ].join("\n")
+  end
+
+  def grub_cfg_contents
+    [
+      "set default=0",
+      "set timeout=10",
+      "set gfxpayload=1024x768x24",
+      "",
+      "menuentry \"Live CD\" {",
+      "  linux  /live/vmlinuz boot=live console=tty0 quiet splash",
+      "  initrd /live/initrd.img",
+      "}",
+      "",
+      "menuentry \"Live CD (debug)\" {",
+      "  linux  /live/vmlinuz boot=live console=tty0 debug",
+      "  initrd /live/initrd.img",
+      "}",
     ].join("\n")
   end
 
